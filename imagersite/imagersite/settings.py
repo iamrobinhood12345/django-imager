45c22478a1827a2359c5041c28ebecbd627e3cf2
Marc Kessler <marc.kesslernyc@gmail.com>
2017-01-19 22:27:54 -0800

    added MEDIA_DIR and added imager_images to apps

--- imagersite/imagersite/settings.py
+++ imagersite/imagersite/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'imager_profile',
     'imagersite',
+    'imager_images',
 ]
 
 MIDDLEWARE = [
@@ -127,6 +128,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/imagersite/static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'MEDIA')
 
 
 #DJANGO-registration
