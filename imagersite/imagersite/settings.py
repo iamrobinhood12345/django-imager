162d0a343a545d55042b19c3b6d54e5b7fcf760b
copenbacon <sclary50@gmail.com>
2017-01-21 15:42:22 -0800

    os.environgit add .git add .git add .git add .!

--- imagersite/imagersite/settings.py
+++ imagersite/imagersite/settings.py
@@ -78,7 +78,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'django_imager',
-        'USER': 'copenbacon',
+        'USER': os.environ['DB_USER'],
         'PASSWORD': os.environ['DB_PASS'],
         'HOST': '127.0.0.1',
         'PORT': '5432',
