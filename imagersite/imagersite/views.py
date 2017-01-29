b7f0423caf19152bb91f4981bafdf9a306ebd987
William Benjamin Shields <ben@Williams-MacBook-Pro.local>
2017-01-28 16:46:06 -0800

    added conditional so you can run the site without user photos.

--- imagersite/imagersite/views.py
+++ imagersite/imagersite/views.py
@@ -6,8 +6,11 @@ from django.conf import settings
 
 def home_page(request, *args, **kwargs):
     photos = Photo.objects.all()
-    idx = randint(0, photos.count() - 1)
-    random_picture = photos[idx]
-    img_url = random_picture.image_file.url
+    if photos:
+        idx = randint(0, photos.count() - 1)
+        random_picture = photos[idx]
+        img_url = random_picture.image_file.url
+    else:
+        img_url = ''
     context = {'img_url': img_url}
     return render(request, 'home.html', context)
