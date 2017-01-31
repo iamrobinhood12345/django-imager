6c309dc13353b668578f907471bad45f8051d482
William Benjamin Shields <ben@Williams-MacBook-Pro.local>
2017-01-30 20:02:24 -0800

    edit profile functionality, url, views, templates, and forms.

--- imagersite/imager_profile/urls.py
+++ imagersite/imager_profile/urls.py
@@ -1,11 +1,13 @@
 from django.conf.urls import url
 from imager_profile.views import (
+    EditProfileView,
     ProfileView,
     OtherUserProfileView
 )
 
 
 urlpatterns = [
+    url(r'^exit$', EditProfileView.as_view(), name='edit_profile'),
     url(r'^$', ProfileView.as_view(), name="profile"),
-    url(r'^(?P<username>\w+)/$', OtherUserProfileView.as_view(), name="user_profile")
+    url(r'^(?P<username>\w+)/$', OtherUserProfileView.as_view(), name="user_profile"),
 ]
