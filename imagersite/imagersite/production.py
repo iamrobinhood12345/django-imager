c953b0ddfd72bd8a447e55324c5bd39070638368
copenbacon <sclary50@gmail.com>
2017-02-01 10:09:21 -0800

    static?

--- imagersite/imagersite/production.py
+++ imagersite/imagersite/production.py
@@ -8,3 +8,12 @@ ALLOWED_HOSTS = ["127.0.0.1",
                  'localhost']
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/imagersite/static/'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+EMAIL_HOST_USER = 'conor.clary@gmail.com'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
+
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_PASS", "")
