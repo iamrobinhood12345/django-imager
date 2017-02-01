ac004c10fd797a585f144f75fba2b74b62a1896d
copenbacon <sclary50@gmail.com>
2017-01-31 17:11:46 -0800

    stuff

--- imagersite/imagersite/settings.py
+++ imagersite/imagersite/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
         'USER': os.environ.get('DB_USER', ''),
         'PASSWORD': os.environ.get('DB_PASS', ''),
         'HOST': os.environ.get('DB_HOST', '127.0.0.1'),
-        'PORT': os.enviorn.get('DB_PORT', '5432'),
+        'PORT': os.environ.get('DB_PORT', '5432'),
         'TEST': {
             'NAME': 'test_imager'
         }
