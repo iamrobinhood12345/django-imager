982383c5fea9368f294ff95a531e65499df5ff6f
Ubuntu <ubuntu@ip-172-31-38-208.us-west-2.compute.internal>
2017-02-02 01:24:28 +0000

    asdkjfasd;kfdaslkfhasdlkjfdasklfhasdljfasdlkfhasdk

--- imagersite/imagersite/production.py
+++ imagersite/imagersite/production.py
@@ -9,13 +9,17 @@ ALLOWED_HOSTS = ["127.0.0.1",
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+#STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'imagersite', 'static')
 STATIC_URL = '/static/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'MEDIA')
+MEDIA_URL = '/media/'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_PASS", "")
-EMAIL_HOST_USER = 'conor.clary@gmail.com'
+EMAIL_HOST_PASSWORD = 'somestupidwordpass'
+EMAIL_HOST_USER = 'codefellowsdjango401imager@gmail.com'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
+SERVER_EMAIL = 'codefellowsdjango401imager@gmail.com'
+DEFAULT_FROM_EMAIL = 'SOURCE SHOT IMAGER SITE BRO. TROLLPY'
