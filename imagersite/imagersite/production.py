95276a7971f3a0d1d42ee9c89b5c6c03ec9d822a
Ubuntu <ubuntu@ip-172-31-38-208.us-west-2.compute.internal>
2017-02-03 01:12:20 +0000

    new hosts

--- imagersite/imagersite/production.py
+++ imagersite/imagersite/production.py
@@ -3,9 +3,9 @@ from imagersite.settings import *
 DEBUG = False
 TEMPLATE_DEBUG = False
 ALLOWED_HOSTS = ["127.0.0.1",
-                 "54.202.162.152",
+                 "54.245.7.225". "54.202.162.152",
                  "ec2-54-202-162-152.us-west-2.compute.amazonaws.com",
-                 'localhost']
+                 'localhost', "ec2-54-245-7-225.us-west-2.compute.amazonaws.com"]
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
